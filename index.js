75457e7fce7aa7b9b18900aab3576589c9b50dba
Soulaiman Assikiou <soulaimanassikiou@gmail.com>
2017-12-06 20:38:40 +0100

    - first component (header) - basic styling - props on component

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+//import a library to helpt to create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+//create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+); 
+
+//render a component
 AppRegistry.registerComponent('albums', () => App);
