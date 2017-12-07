4769547f4fbccd357f9c2832880801ab8219114e
Soulaiman Assikiou <soulaimanassikiou@gmail.com>
2017-12-07 10:55:11 +0100

    - using class components (lifecycle) - installed axios to handdle http requests

---
+++ src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    componentWillMount() {
+        
+    }
+    render() {
+        return (
+            <View>
+                <Text>Album List!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
