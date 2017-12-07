ab6994b7761e3034a19be75083d545cca8ba1a10
Soulaiman Assikiou <soulaimanassikiou@gmail.com>
2017-12-07 19:37:35 +0100

    - albumdetail component created

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
     componentWillMount() {
-        
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+    }
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} albumProp={album} />
+        );
     }
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Album List!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
