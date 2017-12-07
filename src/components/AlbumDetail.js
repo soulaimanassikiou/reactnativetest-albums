b455a96b6e86558e3307e81ef9f2c15e103606df
Soulaiman Assikiou <soulaimanassikiou@gmail.com>
2017-12-07 20:24:10 +0100

    - Card created with styles - Created CardSection component - Passing props between components

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
     return (
-        <View>
-            <Text>{props.albumProp.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <Text>{props.albumProp.title}</Text>
+            </CardSection>
+        </Card>
     );
 };
 
